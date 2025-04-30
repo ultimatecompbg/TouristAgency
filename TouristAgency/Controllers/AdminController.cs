f051a6369218a7f9f44c91d34139563834178523
ultimatecompbg <baystancho0@gmail.com>
2025-04-30 10:23:08 +0300

    admin scaffold + role management

---
+++ TouristAgency/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TouristAgency.Data;
+using TouristAgency.Models;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Users()
+    {
+        var users = await _context.Users.ToListAsync();
+        return View(users);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Approve(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user != null && await _userManager.IsInRoleAsync(user, "TravelAgency"))
+        {
+            user.IsApproved = true;
+            await _userManager.UpdateAsync(user);
+        }
+        return RedirectToAction("Users");
+    }
+}
